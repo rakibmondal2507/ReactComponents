9d1ddb6ec4799aacb77f5f4813ac7d2ded8b61f3
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2024-09-05 19:38:23 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Heading from "./Heading";
+import List from "./List";
+
+// function Heading() {
+//   return <h1>My Favourite Foods</h1>;
+// }
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+      <List />
+    </div>
+  );
+}
+
+export default App;
